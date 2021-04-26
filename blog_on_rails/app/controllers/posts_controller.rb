9c84954ca2b46ee510fe9785627ce14a82d78791
brodyreid <brodyhreid@gmail.com>
2021-04-26 11:50:32 -0700

    homework 6 -part 2

--- blog_on_rails/app/controllers/posts_controller.rb
+++ blog_on_rails/app/controllers/posts_controller.rb
@@ -1,5 +1,7 @@
 class PostsController < ApplicationController
+    before_action :authenticate_user!, except: [:show, :index]
     before_action :find_post, only: [:show, :edit, :update, :destroy]
+    before_action :authorize, only: [:edit, :update, :delete]
 
     def new
         @post = Post.new
@@ -7,7 +9,9 @@ class PostsController < ApplicationController
 
     def create
         @post = Post.new post_params
+        @post.user = current_user
         if @post.save
+            flash[:primary] = "Post created!"
             redirect_to post_path(@post.id)
         else
             render :new
@@ -25,10 +29,15 @@ class PostsController < ApplicationController
     end
 
     def edit
+        unless can?(:edit, @post)
+            flash[:danger]
+            redirect_to root_path
+        end
     end
 
     def update
         if @post.update post_params
+            flash[:dark] = "Post has been updated!"
             redirect_to post_path(@post)
         else
             render :edit
@@ -37,6 +46,7 @@ class PostsController < ApplicationController
 
     def destroy
         @post.destroy
+        flash[:danger] = "Post has been deleted!"
         redirect_to posts_path
     end
 
@@ -48,5 +58,12 @@ class PostsController < ApplicationController
 
     def find_post
         @post = Post.find params[:id]
+    end
+
+    def authorize
+        unless can?(:crud, @idea)
+            flash[:danger] = "Not Authorized"
+            redirect_to root_path
+        end
     end
 end
--- blog_on_rails/config/routes.rb
+++ blog_on_rails/config/routes.rb
@@ -15,7 +15,13 @@ Rails.application.routes.draw do
 
   # delete "/posts/:id" => "posts#destroy"
 
+  root to: redirect(path: "/posts")
+
   resources :posts do
     resources :comments, only: [:create, :destroy], shallow: true
   end
+
+  resources :users, only: [:new, :create]
+
+  resource :session, only: [:new, :create, :destroy]
 end
